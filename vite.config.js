573177ae24c813862b534f9f62fa8af86c997d6b
Rosalind <rosalind.hook@gmail.com>
2025-10-16 09:41:27 +0100

    Add GitLab CI/CD pipeline and Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-app/',      // add for deployment on GitLab pages
   test: {
     globals: true,
     environment: 'happy-dom', // switch from jsdom
