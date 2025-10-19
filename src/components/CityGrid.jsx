02a2aa676b78b56288a5b01aee4a60019da4a796
Rosalind <rosalind.hook@gmail.com>
2025-10-19 11:48:05 +0100

    Add components to replace removed code from WeatherScene.jsx file

---
+++ src/components/CityGrid.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import WeatherCard from './WeatherCard';
+
+const CityGrid = ({ cities, onRemoveCity }) => {
+    return (
+        <Grid container spacing={3} justifyContent="center">
+            {cities.map(cityData => (
+                <WeatherCard
+                    key={cityData.id}
+                    weatherData={cityData.current}
+                    forecastData={cityData.forecast}
+                    cityName={cityData.name}
+                    onRemove={() => onRemoveCity(cityData.id)}
+                />
+            ))}
+        </Grid>
+    );
+};
+
+export default CityGrid;
