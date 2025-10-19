b9ef3709a8276fe6a32006a6b78717ee79a17e2d
Rosalind <rosalind.hook@gmail.com>
2025-10-19 16:03:43 +0100

    Add Nivo arc / gauge component to show current temp

--- src/components/CurrentWeatherView.jsx
+++ src/components/CurrentWeatherView.jsx
@@ -3,14 +3,15 @@ import { CardContent, Typography } from '@mui/material';
 import { getWeatherColour } from '../utils/weatherHelpers';
 import WeatherDataList from './WeatherDataList';
 import CityHeader from './CityHeader';
+import TemperatureGauge from './TemperatureGauge';
 
 const CurrentWeatherView = ({ weatherData }) => {
     const weatherItems = [
-        {
-            term: 'Temperature',
-            value: `${Math.round(weatherData.main.temp)}C`,
-            color: getWeatherColour(weatherData.main.temp)
-        },
+        // {
+        //     term: 'Temperature',
+        //     value: `${Math.round(weatherData.main.temp)}C`,
+        //     color: getWeatherColour(weatherData.main.temp)
+        // },
         {
             term: 'Feels like',
             value: `${Math.round(weatherData.main.feels_like)}C`,
@@ -29,6 +30,7 @@ const CurrentWeatherView = ({ weatherData }) => {
     return (
         <CardContent sx={{ textAlign: 'center', width: '100%' }}>
             <CityHeader weatherData={weatherData} />
+            <TemperatureGauge temperature={weatherData.main.temp} />
             <WeatherDataList items={weatherItems} />
         </CardContent>
     );
